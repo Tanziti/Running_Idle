af23c81f7a6b49bc60aee6343da23f4e200d4be1
Zi Tan <tanzitian3@gmail.com>
2023-10-02 14:37:26 -0400

    express routing done

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,20 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const usersRouter = require('./routes/api/users'); // update the import file path
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const app = express();
+
+app.use(logger('dev')); // log request components (URL/method) to terminal
+app.use(express.json()); // parse JSON request body
+app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
+app.use(cookieParser()); // parse cookies as an object on req.cookies
+
+// Attach Express routers
+
+app.use('/api/users', usersRouter); // update the path
 
 module.exports = app;
