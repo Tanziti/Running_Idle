1ae15aed627a489a930da167abf3a2224e985599
Avery Berry <AveryRBerry@gmail.com>
2023-10-03 13:51:36 -0400

    fixed seeds buggs

--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -2,19 +2,20 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 const { mongoURI: db } = require('../config/keys.js');
 const User = require('../models/User');
+const Character = require('../models/Character.js')
 
 const bcrypt = require('bcryptjs');
 const { faker } = require('@faker-js/faker');
 
 
-const NUM_SEED_USERS = 10;
-const NUM_SEED_TWEETS = 30;
-
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to MongoDB successfully');
-    insertSeeds();
+    insertSeeds(); 
+  })
+  .then(() => {
+    insertSeeds2();
   })
   .catch(err => {
     console.error(err.stack);
@@ -22,6 +23,7 @@ mongoose
   });
 // Create users
 const users = [];
+const characters = [];
 
 users.push(
     new User ({
@@ -61,6 +63,136 @@ users.push(
         hashedPassword: bcrypt.hashSync('password', 10)
     })
     )
+
+characters.push(
+    new Character ({
+        user: users[1].id,
+        name: 'TheBestBob',
+        heart: 0,
+        legs: 0,
+        arms: 0,
+        points: 0,
+        outfit: 'red',
+        shoes: 'yellow'
+    })
+    )
+
+characters.push(
+    new Character ({
+        user: users[2].id,
+        name: 'Bob',
+        heart: 0,
+        legs: 0,
+        arms: 0,
+        points: 0,
+        outfit: 'red',
+        shoes: 'yellow'
+    })
+    )
+
+characters.push(
+    new Character ({
+        user: users[2].id,
+        name: 'Tim',
+        heart: 0,
+        legs: 0,
+        arms: 0,
+        points: 0,
+        outfit: 'yellow',
+        shoes: 'red'
+    })
+    )
+
+    characters.push(
+    new Character ({
+        user: users[3].id,
+        name: 'GarboBob',
+        heart: 0,
+        legs: 0,
+        arms: 0,
+        points: 0,
+        outfit: 'red',
+        shoes: 'yellow'
+    })
+    )
+
+characters.push(
+    new Character ({
+        user: users[3].id,
+        name: 'SuperBob',
+        heart: 0,
+        legs: 0,
+        arms: 0,
+        points: 0,
+        outfit: 'red',
+        shoes: 'yellow'
+    })
+    )
+
+characters.push(
+    new Character ({
+        user: users[3].id,
+        name: 'ThatBob',
+        heart: 0,
+        legs: 0,
+        arms: 0,
+        points: 0,
+        outfit: 'red',
+        shoes: 'yellow'
+    })
+    )
+
+characters.push(
+    new Character ({
+        user: users[4].id,
+        name: 'GodBob',
+        heart: 0,
+        legs: 0,
+        arms: 0,
+        points: 0,
+        outfit: 'red',
+        shoes: 'yellow'
+    })
+    )
+
+characters.push(
+    new Character ({
+        user: users[4].id,
+        name: 'BobOfGod',
+        heart: 0,
+        legs: 0,
+        arms: 0,
+        points: 0,
+        outfit: 'red',
+        shoes: 'yellow'
+    })
+    )
+
+characters.push(
+    new Character ({
+        user: users[4].id,
+        name: 'Rob',
+        heart: 0,
+        legs: 0,
+        arms: 0,
+        points: 0,
+        outfit: 'red',
+        shoes: 'yellow'
+    })
+    )
+
+characters.push(
+    new Character ({
+        user: users[4].id,
+        name: 'BoB',
+        heart: 0,
+        legs: 0,
+        arms: 0,
+        points: 0,
+        outfit: 'red',
+        shoes: 'yellow'
+    })
+    )
         
 const insertSeeds = () => {
  console.log("Resetting db and seeding users and tweets...");
@@ -69,6 +201,21 @@ const insertSeeds = () => {
     .then(() => User.insertMany(users))
     .then(() => {
     console.log("Done!");
+    mongoose.disconnect();
+    })
+        .catch(err => {
+        console.error(err.stack);
+        process.exit(1);
+     });
+
+}
+
+const insertSeeds2 = () => {
+          
+  Character.collection.drop()
+    .then(() => Character.insertMany(characters))
+    .then(() => {
+    console.log("Done with characters!");
      mongoose.disconnect();
     })
         .catch(err => {
