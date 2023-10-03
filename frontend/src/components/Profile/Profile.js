6ac634cd050ac9eea7d49e3013048a79241c436e
Avery Berry <AveryRBerry@gmail.com>
2023-10-02 23:57:16 -0400

    created reducer and starting charicter profile page

---
+++ frontend/src/components/Profile/Profile.js
@@ -0,0 +1,35 @@
+// src/components/Profile/Profile.js
+
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUserCharacter, clearCharacterErrors } from '../../store/characters';
+import CharacterItem from '../Characters/CharacterItem';
+
+function Profile () {
+    const dispatch = useDispatch();
+    const currentUser = useSelector(state => state.session.user);
+    const userCharacters = useSelector(state => Object.values(state.characters.user))
+    
+    useEffect(() => {
+        dispatch(fetchUserCharacter(currentUser._id));
+        return () => dispatch(clearCharacterErrors());
+    }, [currentUser, dispatch]);
+
+    if (userCharacters.length === 0) {
+        return <button>New Game</button>;
+    } else {
+        return (
+        <>
+            <h2>All of {currentUser.username}'s Characters</h2>
+            {userCharacters.map(character => (
+            <CharacterItem
+                key={character._id}
+                character={character}
+            />
+            ))}
+        </>
+        );
+    }
+}
+
+export default Profile;
