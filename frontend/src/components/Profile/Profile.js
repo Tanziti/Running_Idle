12183b315cdb9def2e4b9034d3937c5ee9b6def6
Avery Berry <AveryRBerry@gmail.com>
2023-10-03 22:27:01 -0400

    added demo user button, and logout for char index page

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,29 +1,40 @@
-// src/components/Profile/Profile.js
-
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUserCharacter, clearCharacterErrors } from '../../store/characters';
 import CharacterItem from '../Characters/CharacterItem';
+import { logout } from '../../store/session';
+import { useHistory } from "react-router-dom";
 
 function Profile () {
     const dispatch = useDispatch();
     const currentUser = useSelector(state => state.session.user);
     const userCharacters = useSelector(state => Object.values(state.characters.user))
+    const history = useHistory();
 
     useEffect(() => {
-        dispatch(fetchUserCharacter(currentUser._id));
+        if (currentUser) {
+            dispatch(fetchUserCharacter(currentUser._id));
+        }
         return () => dispatch(clearCharacterErrors());
     }, [currentUser, dispatch]);
 
-// console.log('cool***********', userCharacters)
-
-
+    const logoutUser = e => {
+        e.preventDefault();
+        dispatch(logout());
+        history.push("/");
+    }
 
     if (userCharacters.length === 0) {
-        return <button>New Game</button>
+        return (
+            <>
+                <button onClick={logoutUser} >Logout</button>
+                <button>New Game</button>
+            </>
+        )
     } else {
         return (
         <>
+            <button onClick={logoutUser} >Logout</button>
             <h2>All of {currentUser.username}'s Characters</h2>
             <button>New Game</button>
             {userCharacters.map(character => (
