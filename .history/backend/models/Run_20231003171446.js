49047ac0bf1357629958994a3bb0796db3102e71
ajb-4 <alexbrown4711@gmail.com>
2023-10-03 22:24:04 -0400

    do validations

---
+++ .history/backend/models/Run_20231003171446.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const runSchema = new Schema({
+    character: {
+        type: Schema.Types.ObjectId,
+        ref: 'Character'
+    },
+    startTime: {
+        type: Number,
+        required: true
+    },
+    endTime: {
+        type: Number
+    },
+    startPosition: {
+        type: [Number],
+        required: true,
+        validate: {
+          validator: function (value) {
+            return Array.isArray(value) && value.length === 2;
+          },
+          message: 'Invalid coordinates format for startPosition.'
+        }
+      },
+    endPosition: {
+        type: [Number]
+      },
+    duration: {
+        type: Number
+    },
+    distance: {
+        type: Number 
+    }
+ });
+
+ module.exports = mongoose.model('Run', runSchema);
