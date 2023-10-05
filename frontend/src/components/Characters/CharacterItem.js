2ee25f34dfe56090f1fd8ad09411c224d9913693
Muhammad Amray <muhammadamray@gmail.com>
2023-10-05 16:27:02 -0400

    fixing index but not yet complete

--- frontend/src/components/Characters/CharacterItem.js
+++ frontend/src/components/Characters/CharacterItem.js
@@ -1,14 +1,11 @@
-import './CharacterItem.css';
-import { Link } from 'react-router-dom';
-import { deleteCharacter } from '../../store/characters'
-import { useDispatch } from 'react-redux';
+import "./CharacterItem.css";
+import { Link } from "react-router-dom";
+import { deleteCharacter } from "../../store/characters";
+import { useDispatch } from "react-redux";
 // import { useEffect, useState } from 'react';
 // import { useHistory } from 'react-router-dom';
 
-
-
-function CharacterItem ({character}) { 
-  
+function CharacterItem({ character }) {
   // const history = useHistory();
   const dispatch = useDispatch();
 
@@ -16,27 +13,23 @@ function CharacterItem ({character}) {
     e.preventDefault();
     dispatch(deleteCharacter(character._id));
     // history.push('/user/characters');
-    window.location.reload()//needs to be fixed ....
+    window.location.reload(); //needs to be fixed ....
   };
 
-  
-
-
   return (
-    <Link to={`/character/${character._id}`}>
-      <div className="character">
-        <ul id='charStuff'>
-          <button onClick={handleDeleteCharacter}>Delete</button>
-          name<li>{character.name}</li>
-          arms<li>{character.arms}</li>
-          heart<li>{character.heart}</li>
-          legs<li>{character.legs}</li>
-          outfit<li>{character.outfit}</li>
-          points<li>{character.points}</li>
-          shoes<li>{character.shoes}</li>
-        </ul>
-      </div>
-    </Link>
+    <div className="form-cont">
+      <Link id="linkss" to={`/character/${character._id}`}>
+        <div className="character">
+          <div className="char-name">{character.name}</div>
+          <div className="char-points">Points:{character.points}</div>
+          <div>
+            <button className="del-btn" onClick={handleDeleteCharacter}>
+              Delete
+            </button>
+          </div>
+        </div>
+      </Link>
+    </div>
   );
 }
 
