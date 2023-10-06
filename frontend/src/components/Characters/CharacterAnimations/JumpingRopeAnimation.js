af94c83396a5e777fd032f1227ba3e68663d2507
Avery Berry <AveryRBerry@gmail.com>
2023-10-06 00:22:24 -0400

    worked on styling char show page

--- frontend/src/components/Characters/CharacterAnimations/JumpingRopeAnimation.js
+++ frontend/src/components/Characters/CharacterAnimations/JumpingRopeAnimation.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-// import './CharactersIcon.css';
 
-function CharactersIcon() {
+const JumpingRopeAnimation = ({character})  => {
   const [isImage1Visible, setIsImage1Visible] = useState(true);
 
+  console.log(character.outfit)
   useEffect(() => {
     const interval = setInterval(() => {
       setIsImage1Visible((prevIsImage1Visible) => !prevIsImage1Visible);
@@ -17,12 +17,14 @@ function CharactersIcon() {
       <div>
         {isImage1Visible ? (
           <img
-            src="/assets/Green_Outfit/Green_Outfit_Green_Shoes4.png"
+            id='CharacterIcon'
+            src={`/assets/${character.outfit}_Outfit/${character.outfit}_Outfit_${character.shoes}_Shoes4.png`}
             alt="Character1"
           />
         ) : (
           <img
-            src="/assets/Green_Outfit/Green_Outfit_Green_Shoes5.png"
+            id='CharacterIcon'
+            src={`/assets/${character.outfit}_Outfit/${character.outfit}_Outfit_${character.shoes}_Shoes5.png`}
             alt="Character2"
           />
         )}
@@ -31,4 +33,4 @@ function CharactersIcon() {
   );
 }
 
-export default CharactersIcon;
+export default JumpingRopeAnimation;
