e80b36efa16220082f85eae64f22170795534d24
ajb-4 <alexbrown4711@gmail.com>
2023-10-08 20:15:38 -0400

    do everything but add points

--- frontend/src/components/RunsPage/RunsPage.js
+++ frontend/src/components/RunsPage/RunsPage.js
@@ -96,6 +96,39 @@ const getEndLocation = () => {
     }
   }
 
+  const formatTime = (millisec) => {
+    const minutes = Math.floor((millisec / (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((millisec % (1000 * 60)) / 1000);
+
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    return `${minutes}:${formattedSeconds}`
+  }
+
+  const calculateDistance = (lat1, lon1, lat2, lon2) => {
+    const R = 3958.8;
+  
+    const dLat = deg2rad(lat2 - lat1);
+    const dLon = deg2rad(lon2 - lon1);
+  
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+              Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+              Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c;
+  
+    return distance;
+  }
+  
+  const deg2rad = (deg) => {
+    return deg * (Math.PI / 180);
+  }
+
+  const getPace = () => {
+    
+  }
+
   useEffect(() => {
    createRun();
   }, [endLng]);
@@ -152,8 +185,12 @@ const getEndLocation = () => {
                             {toggleRunStart}
                           </div>
                           <div id='currentrun-data'>
-                            <div>Start Lat: {startLat} Start Lng: {startLng}</div>
-                            <div>End Lat: {endLat} End Lng: {endLng}</div>
+                            <div>Start Position: {startLng ? `[${Number(startLat.toFixed(4))}, ${Number(startLng.toFixed(4))}]` : ''}</div>
+                            <div>End Position: {endLng ? `[${Number(endLat.toFixed(4))}, ${Number(endLng.toFixed(4))}]` : ''}</div>
+                            <div>Time: {endTime ? formatTime(endTime - startTime) : ''}</div>
+                            <div>Distance: {endLng ? calculateDistance(startLat, startLng, endLat, endLng) : ''}mi</div>
+                            <div>Pace: {endLng ? formatTime(Math.floor((endTime - startTime)/calculateDistance(startLat, startLng, endLat, endLng))) : ''} time/mile</div>
+                            <div>Points: {endLng ? Number((15 * calculateDistance(startLat, startLng, endLat, endLng)).toFixed(3)) : ''} pts</div>
                           </div>
 
                         
